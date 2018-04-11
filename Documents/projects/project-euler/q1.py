27ef557932142075b9b927576e0b4fead0972e5c
Joe Yelland <joeyelland@gmail.com>
2018-04-11 20:27:36 +0100

    Update q1.py

--- Documents/projects/project-euler/q1.py
+++ Documents/projects/project-euler/q1.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
+i = 0 #counter
+total = 0 #result
 
-def helloworld(yolo):
-    return yolo
+while (i < 1000): #loop through numbers below 1000
+    if ((i % 3 == 0) or (i % 5 == 0)): #true if i is a multiple of 3 or 5 or both
+        total += i #adds value of i to total
+        i += 1 
+    else:
+        i += 1
 
-print helloworld('Is this python?')
+print total
