52471a9df30fb0b5539616ef784f2619047aa130
Joe Yelland <joeyelland@gmail.com>
2018-04-11 21:31:16 +0100

    Update q2.py

--- Documents/projects/project-euler/q2.py
+++ Documents/projects/project-euler/q2.py
@@ -7,6 +7,7 @@ fibo_arr = [1, 2]
 
 while (next_num <= 4000000):
     next_num = fibo_arr[i] + fibo_arr[k]
+    fibo_arr.append(next_num)
     if (next_num % 2 == 0):
         total += next_num
         i += 1
