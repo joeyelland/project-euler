1a49635e1920b09c94bf1cff3fae8778f42b9292
Joe Yelland <joeyelland@gmail.com>
2018-04-15 22:21:34 +0100

    Created a check product function to find highest 3-digit number product

--- q4.py
+++ q4.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python
 
-n = str(int(input("input n = ")))
-
 def find_palindrome(n): #Outputs true if number is a palindrome
     num = str(n)
-    if (num == num[::-1]):
+    if (num == num[::-1]): #Checks if the number is a palindrome
         return True
     else:
         return False
 
-print find_palindrome(n)
+def product_check(n): # Runs through all possible 3-digits combinations until n is found
+    for x in range(999,100,-1):
+        for y in range(x, 100, -1):
+            if(x * y == n):
+                return True
 
-'''def find_largest_num (n):
+def find_largest_num (n):
     x = 999 * 999 # Largest product of 3-digit numbers
-    while (x > (99 * 99)):
-        if (find_palindrome(n)):
-            #find factors
+    y = 99 * 99 # ;Largest product of 2-digit numbers
+    while (x > y):
+        if (find_palindrome(n) and product_check(n)):
+            return n
         else:
-            x -= 1
-    return n'''
+            n = n - 1
+            x = x - 1
+
+print find_largest_num(999 * 999)
