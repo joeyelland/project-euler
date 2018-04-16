844ca686aaf210282db3a1bcd4ea6a2ccdc42081
Joe Yelland <joeyelland@gmail.com>
2018-04-16 15:23:15 +0100

    Completed, but still a slow solution

--- q7.py
+++ q7.py
@@ -8,6 +8,9 @@ def prime_counter(n):
   prime_count = 0
   for i in range(1, n + 1):
     if all(i % j != 0 for j in range(2, i)):
-      print(str(i) + " is a prime")
+      prime_count += 1
+      print(prime_count)
+      if (prime_count == 10002):
+        return i
 	
-print(prime_counter(10001))
+print(prime_counter(5000000))
