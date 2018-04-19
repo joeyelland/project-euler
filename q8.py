38cf2bc1e6341966c327d11323fb3ea616100734
Joe Yelland <joeyelland@gmail.com>
2018-04-19 15:59:19 +0100

    in progress

--- q8.py
+++ q8.py
@@ -24,40 +24,40 @@ digits = '''73167176531330624919225119674426574742355349194934
 71636269561882670428252483600823257530420752963450'''
 
 def find_adjacent(n, i):
-  cond_1 = int(n[i])
-  cond_2 = int(n[i - 1]) + 1
-  cond_3 = int(n[i - 1]) - 1
-  cond_4 = int(n[i - 1])
-  cond_5 = int(n[i + 1])
-  
-  state_1 = 1 == 0 #checks if it is the start of the string
-  state_2 = cond_1 == cond_4 #checks if next number is equal
-  state_3 = cond_1 == cond_2 #checks if next number is adjacent by + 1
-  state_4 = cond_1 == cond_3 #checks if next number is adjacent by - 1
-  #state_5 = cond_5 != (cond_1 or cond_2 or cond_3) #checks if the previous number was not adjacent
-  
-  if(state_1 or state_2 or state_3 or state_4 ):
-    return n[i]
-  else:
-    return False
-    
-def check_length(n):
-  if(len(n) == 13):
-    return True
-  else:
-    return False
+	cond_1 = int(n[i])
+	cond_2 = int(n[i - 1]) + 1
+	cond_3 = int(n[i - 1]) - 1
+	cond_4 = int(n[i - 1])
+	cond_5 = int(n[i + 1])
+	
+	state_1 = 1 == 0 #checks if it is the start of the string
+	state_2 = cond_1 == cond_4 #checks if next number is equal
+	state_3 = cond_1 == cond_2 #checks if next number is adjacent by + 1
+	state_4 = cond_1 == cond_3 #checks if next number is adjacent by - 1
+	
+	if(state_1 or state_2 or state_3 or state_4):
+		return n[i]
+	else:
+		return False
+
+def find_product(n):
+	n = 1
+	for x in n:
+		n += x
+	return n
 
 def find_sequence(n):
-  adjacent_numbers = []
-  j = 0
-  while j + 1 < len(n):
-    if(find_adjacent(n, j)):
-      adjacent_numbers.append(find_adjacent(n, j))
-      print adjacent_numbers
-      j += 1
-    else:
-      j += 1
-  return adjacent_numbers
-  
-#print find_adjacent('179376', 0)
+	adjacent_array = [] #Holds the sequence of adjacent numbers
+	product_array = [] #Holds all the product of 13 digits sequences of adjacent numbers
+
+	j = 0
+	while j + 1 < len(n):
+		if(find_adjacent(n, j)): #Find out if the next number is adjacent
+			adjacent_array.append(find_adjacent(n, j)) #The adjacent number
+		elif(len(adjacent_array) == 13): #Checks if sequence is of length 13
+			product_array.append(find_product(n)) #Appends product of number if length is 13
+		else:
+			del adjacent_array #Deletes the array if the next number is adjacent and if the array isn't of length 13
+
 print find_sequence('12345678912345689')
+
